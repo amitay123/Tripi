30213bdf49bb8ecc2f46fb53d0ab0ac931a90035
Amitay Tzanani <amitay@MacBook-Pro-sl-Amitay.local>
2026-04-27 17:40:26 +0300

    Manual firebase deployment for better reliability

--- push_fix.sh
+++ push_fix.sh
@@ -1,4 +1,4 @@
 cd /Users/amitay/tripi
 git add .
-git commit -m "Fix workflow syntax: use PROJECT_PATH instead of working-directory"
+git commit -m "Manual firebase deployment for better reliability"
 git push
